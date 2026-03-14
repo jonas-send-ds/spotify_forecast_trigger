cc70e1f901ac8a9bbbb14c0934dd210324bad8b9
jonas-send <datascience@jonassend.com>
2026-03-14 15:54:07 +0100

    add spreadsheet data #13

--- lambda/index.py
+++ lambda/index.py
@@ -1,11 +1,15 @@
 
 import os
 import requests
+from typing import Iterable
 from typing import Any
 
 import boto3
+import gspread
 import polars as pl
 from dotenv import load_dotenv
+
+# TODO #15: move to pipeline repo to make coupling explicit and avoid copied code
 
 API_URL = "https://api.songstats.com/enterprise/v1/artists/historic_stats"
 ARTISTS = {
@@ -15,7 +19,22 @@ ARTISTS = {
 }
 BUCKET = "spotify-forecast"
 COLUMNS = ["artist", "date", "monthly_listeners", "reach"]
+# TODO: find better way to handle this
+GOOGLE_CREDENTIALS = {
+  "type": "service_account",
+  "project_id": "david-spotify-forecast",
+  "private_key_id": "96c5bb633292dcbc6d7f0a0552483c21325f5de9",
+  "client_email": "spotify-forecast-account@david-spotify-forecast.iam.gserviceaccount.com",
+  "client_id": "117941236911552114013",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/spotify-forecast-account%40david-spotify-forecast.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
 LATEST_DATA = "latest_data.parquet"
+PRIVATE_KEY_IS_NOT_SET = "GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY environment variable is not set"
+SPREADSHEET_ID = "167UTVu2XVAM0MlGw-Cpw0tcMyuphC3ifJpOiC_y_a74"
 
 load_dotenv()
 s3 = boto3.client("s3")
@@ -56,9 +75,9 @@ def load_songstats_data(artists: dict[str, str]) -> pl.DataFrame:
 
     df = fix_anomalies(df)
 
-    # We interpret the values as lagged by one day
+    # We interpret the monthly listeners values as lagged by one day
     return df.with_columns(
-        pl.col("date") - pl.duration(days=1)
+        pl.col("monthly_listeners").shift(-1).over("artist")
     )
 
 
@@ -83,23 +102,84 @@ def fix_anomalies(df: pl.DataFrame) -> pl.DataFrame:
     )
 
 
-def load_data() -> (pl.DataFrame, pl.DataFrame):
+def get_credentials() -> dict:
     """
-    Loads and processes song statistics data, and a latest dataset
-    containing the most recent data for each artist.
+    Retrieve the Google service account credentials.
+
+    :return: A dictionary containing the credentials-data.
+    """
+    print("Loading Google Service Account credentials...")
+    credentials = GOOGLE_CREDENTIALS.copy()
+
+    load_dotenv()
+    private_key_env = os.getenv("GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY")
+
+    if not private_key_env:
+        raise ValueError(PRIVATE_KEY_IS_NOT_SET)
+
+    # HACKY: Replace literal \n with actual newline characters
+    credentials["private_key"] = private_key_env.replace("\\n", "\n")
+
+    return credentials
+
+
+def load_spreadsheet_data(artists: Iterable[str]) -> pl.DataFrame:
+    """
+    Loads and processes data from a Google Spreadsheet containing monthly listener
+    statistics for specified artists.
+
+    :param artists: A list of artist names to select specific columns from the spreadsheet.
+    :return: A Polars DataFrame containing the processed data with the following
+        columns: date, artist, and monthly_listeners. The DataFrame is sorted by
+        date and artist, with missing values removed.
+    """
+    credentials = get_credentials()
+    client = gspread.service_account_from_dict(credentials)
+
+    print("Loading and processing data from Google Spreadsheet...")
+    public_sheet: gspread.Spreadsheet = client.open_by_key(SPREADSHEET_ID)
+    worksheet = public_sheet.worksheet("Monthly Listeners")
+    data = worksheet.get_all_values()  # list of lists
+
+    df = pl.DataFrame(
+        data[1:],
+        schema=data[0],  # first row as column names
+        orient="row",
+    )
+
+    return (df.select(["Date", *[x for x in artists]])
+            .rename({"Date": "date"})
+            .unpivot(index="date", variable_name="artist", value_name="monthly_listeners")
+            .with_columns(
+                pl.col("date").str.to_date("%m/%d/%Y"),
+                pl.col("monthly_listeners").str.replace_all(",", "").replace("", None).cast(pl.Int64))
+            .drop_nulls()
+            .sort(["date", "artist"]))
+
+
+def load_data() -> pl.DataFrame:
+    """
+    Loads and processes a latest dataset containing the most recent data for each artist.
 
     :return: df_latest: dataset containing the most recent data for each artist
     """
-    # df_spreadsheet = load_spreadsheet_data(ARTISTS.keys())  # TODO #11: is spreadsheet data sufficiently more up-to-date to justify additional complexity?
-    df = load_songstats_data(ARTISTS).select(["date", "artist", "monthly_listeners", "reach"])
+    df_songstats = load_songstats_data(ARTISTS).select(["date", "artist", "monthly_listeners", "reach"])
+    df_spreadsheet = load_spreadsheet_data(ARTISTS.keys())
 
-    df_latest = (df
+    df = pl.concat([df_songstats, df_spreadsheet], how="diagonal_relaxed")
+
+    # use monthly listeners data from spreadsheet (when available) and songstats data for reach
+    # TODO: make more robust to sorting of dataframes
+    df = (df.group_by(["date", "artist"]).agg(
+        pl.col("monthly_listeners").last(),
+        pl.col("reach").first(),
+    ).sort(["date", "artist"]))
+
+    return (df
                  .sort("date")
                  .with_columns(pl.all().fill_null(strategy="forward").over("artist"))
                  .group_by("artist")
                  .last())
-
-    return df_latest
 
 
 # ruff: noqa: ANN401 - typing for unused parameters is unimportant
